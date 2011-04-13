1c183eac1ced9b6fb30a7f3ae677248e0a0dd50b
Igor Vaynberg <igor.vaynberg@gmail.com>
2011-04-13 15:04:50 -0700

    adding dummy test to make bamboo happy

---
+++ wicket-fullcalendar-core/src/test/java/net/ftlines/wicket/fullcalendar/DummyTest.java
@@ -0,0 +1,18 @@
+package net.ftlines.wicket.fullcalendar;
+
+import org.junit.Test;
+
+/**
+ * Dummy test to make bamboo happy because its fails if it doesnt find any test artifacts
+ * 
+ * @author igor
+ * 
+ */
+public class DummyTest
+{
+	@Test
+	public void dummy()
+	{
+
+	}
+}
