2a5786d7297fd6d49e4141704f6d993e28ea3ff9
Igor Vaynberg <igor.vaynberg@42lines.net>
2017-06-20 18:06:35 -0700

    fix javadoc

--- wicket-fullcalendar-examples/src/main/java/net/ftlines/wicket/fullcalendar/WicketApplication.java
+++ wicket-fullcalendar-examples/src/main/java/net/ftlines/wicket/fullcalendar/WicketApplication.java
@@ -15,10 +15,7 @@ package net.ftlines.wicket.fullcalendar;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
- * Application object for your web application. If you want to run this application without deploying, run the Start
- * class.
- * 
- * @see net.ftlines.wicket.fullcalendar.StartFullCalendarExamples#main(String[])
+ * Application object for your web application.
  */
 public class WicketApplication extends WebApplication {
 	/**
